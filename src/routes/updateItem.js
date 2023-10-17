2790dc2c5b7b8836f2f28657c37319b4973e7263
molly-100 <125601191+molly-100@users.noreply.github.com>
2023-10-17 21:58:26 +0100

    Update updateItem.js
    updateItem.js added favourite code

--- src/routes/updateItem.js
+++ src/routes/updateItem.js
@@ -1,10 +1,24 @@
 const db = require('../persistence');
 
 module.exports = async (req, res) => {
-    await db.updateItem(req.params.id, {
+    const updatedFields = {
         name: req.body.name,
         completed: req.body.completed,
-    });
+        favorite: req.body.favorite, // Add support for 'favorite'
+    };
+
+    await db.updateItem(req.params.id, updatedFields);
     const item = await db.getItem(req.params.id);
     res.send(item);
 };
+
+// const db = require('../persistence');
+
+// module.exports = async (req, res) => {
+//     await db.updateItem(req.params.id, {
+//         name: req.body.name,
+//         completed: req.body.completed,
+//     });
+//     const item = await db.getItem(req.params.id);
+//     res.send(item);
+// };
